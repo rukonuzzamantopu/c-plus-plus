2dad59e9375c9fc7dc6066fb071c999bae949733
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2024-11-17 01:35:10 +0600

    add loop part

---
+++ loop/loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<5;i++){
+    cout<<i<<endl;
+    }
+    int i=0;
+    while(i<5){
+        cout<<i<<endl;
+        i++;
+    }
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<4);
+    return 0;
+
+}
