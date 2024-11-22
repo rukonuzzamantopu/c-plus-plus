c34f3ff22ffd040c04a108da5d953fcf479b8724
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2024-11-23 02:14:45 +0600

    add dynamic allocaion aprt

---
+++ dynamic allocation/1st.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int main()
+{
+    int* ptr;
+    ptr = new int;
+    *ptr = 10;
+    cout << "Address: " << ptr << endl;
+    cout << "Value: " << *ptr;
+
+    return 0;
+}
